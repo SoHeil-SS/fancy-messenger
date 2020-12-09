886ca959663cea5feeeae7ded31690a325e4cca7
dev.soheil.ss@gmail.com <dev.soheil.ss@gmail.com>
2020-12-09 03:33:57 +0330

    vercel fix

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import { render } from "react-dom";
 import React from "react";
 
 import App from "./App";
-import Portal from "./Components/Others/Portal";
 
 import "react-toastify/dist/ReactToastify.css";
 import "react-contexify/dist/ReactContexify.min.css";
